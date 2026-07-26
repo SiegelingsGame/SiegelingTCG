27b8f0341eddecf6af131eceb93c71db12f198c4
SiegelingsGame <siegelingsgame@gmail.com>
2026-07-26 18:30:28 -0400

    Recover Shop packs after catalog failures

--- src/test/java/com/sieglings/service/PackCatalogServiceTest.java
+++ src/test/java/com/sieglings/service/PackCatalogServiceTest.java
@@ -113,6 +113,16 @@ class PackCatalogServiceTest {
     }
 
     @Test
+    void dailyOfferSerializationAndSortingHandleNullElements() throws Exception {
+        PackCatalogService service = createService(new NullElementCardDefinitions());
+
+        List<Map<String, Object>> offers = service.serializeDailyOffers();
+
+        assertEquals(5, offers.size());
+        assertTrue(offers.stream().anyMatch(offer -> offer.get("element") == null));
+    }
+
+    @Test
     void dailyOfferPricesScaleByRarityAndChargeMoreForKnights() throws Exception {
         PackCatalogService service = createService(new PricedRotationCardDefinitions());
 
@@ -225,6 +235,34 @@ class PackCatalogServiceTest {
                     "trainer-fire",
                     "Fire Knight",
                     Element.FIRE,
+                    Rarity.RARE,
+                    Ability.passive("Banner", "Allies gain +1", "damage_boost", 1),
+                    Ability.damage("Strike", "Deal 2 damage", TargetType.SINGLE_ENEMY, null, 1, 2),
+                    false
+            ));
+        }
+    }
+
+    private static class NullElementCardDefinitions extends FireOnlyCardDefinitions {
+        @Override
+        public List<Card> getDeckBuilderCatalog() {
+            return List.of(
+                    new SieglingCard("null-siegling", "Unaligned Unit", null, Rarity.COMMON, 7, 3, List.of(), Row.FRONT),
+                    fireSiegling("fire-b", Row.MIDDLE),
+                    fireSiegling("fire-c", Row.BACK),
+                    new SpellCard("fire-spell", "Fire Spell", Element.FIRE, Rarity.COMMON, 1,
+                            Ability.damage("Spark", "Deal 1 damage", TargetType.SINGLE_ENEMY, null, 1, 1)),
+                    new TrapCard("fire-trap", "Fire Trap", Element.FIRE, Rarity.COMMON, Element.FIRE, 1,
+                            Ability.damage("Snare", "Deal 1 damage", TargetType.SINGLE_ENEMY, null, 1, 1))
+            );
+        }
+
+        @Override
+        public List<TrainerCard> getTrainerOptions() {
+            return List.of(new TrainerCard(
+                    "trainer-null",
+                    "Unaligned Knight",
+                    null,
                     Rarity.RARE,
                     Ability.passive("Banner", "Allies gain +1", "damage_boost", 1),
                     Ability.damage("Strike", "Deal 2 damage", TargetType.SINGLE_ENEMY, null, 1, 2),
